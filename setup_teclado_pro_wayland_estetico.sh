30d2fbec3cc9a640cb34940d2e042381dcfc8c0b
Jorge humberto espinosa Salvatella <134461073+jorgesalvatella@users.noreply.github.com>
2025-03-01 10:59:59 -0500

    Fix: Registro correcto del atajo personalizado para lanzar wofi (Super + Espacio)

--- setup_teclado_pro_wayland_estetico.sh
+++ setup_teclado_pro_wayland_estetico.sh
@@ -89,6 +89,7 @@ if ! grep -q "neofetch" "$HOME/.bashrc"; then
 fi
 
 # 8. Atajos de teclado (con wofi)
+dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings "['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/']"
 dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/name "'Abrir wofi'"
 dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/command "'wofi --show drun'"
 dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/binding "'<Super>space'"
